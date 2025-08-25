30b2c20219aeb4bfb60124cc1f7e6eae79fd850d
Kelvin Odoemene <kelvin.odoemene@gmail.com>
2025-08-25 05:17:19 +0000

    Implement this patch
    You fixed Storage; now Next.js is blocking the rem

---
+++ src/next.config.ts
@@ -0,0 +1,30 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
